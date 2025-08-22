3761858f3fc0336bb2d2f610cf40bd96d308a1be
Komal Kumari <kskomal7819@gmail.com>
2025-08-22 15:25:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Simple click alert for buttons
+// document.querySelectorAll('.btn, .btn-outline, .btn-small').forEach(button => {
+//     button.addEventListener('click', e => {
+//         // e.preventDefault();
+//         // alert('This is a demo portfolio. Links are not active.');
+//     });
+// });
+
+// Mobile menu toggle
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+
+
+
+
+
+// document.querySelector(".contact-form").addEventListener("submit", function(e) {
+//     // e.preventDefault();
+//     alert("Your message has been sent!");
+// });
